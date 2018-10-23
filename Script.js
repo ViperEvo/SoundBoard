1ef882b02c4fae36b0be537856d45c2a49e2dae2
Szupryczyński Arkadiusz <arkadiusz.szupryczynski@microsoft.wsei.edu.pl>
2018-10-23 16:09:24 +0200

    Podstawka :)

---
+++ Script.js
@@ -0,0 +1,79 @@
+document.body.addEventListener('keypress', (e)=>{
+    
+    
+    if (e.charCode == 113) 
+    {
+        let sClap = document.querySelector('#sBoom')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 119) 
+    {
+        let sClap = document.querySelector('#sClap')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 101) 
+    {
+        let sClap = document.querySelector('#sHiHat')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 114) 
+    {
+        let sClap = document.querySelector('#sKick')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 97) 
+    {
+        let sClap = document.querySelector('#sMyMan')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 115) 
+    {
+        let sClap = document.querySelector('#sOpenHat')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 100) 
+    {
+        let sClap = document.querySelector('#sPickleRick')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 102) 
+    {
+        let sClap = document.querySelector('#sRide')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 122) 
+    {
+        let sClap = document.querySelector('#sSnap')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 120) 
+    {
+        let sClap = document.querySelector('#sSnare')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 99) 
+    {
+        let sClap = document.querySelector('#sTink')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    if (e.charCode == 118) 
+    {
+        let sClap = document.querySelector('#sTom')
+        sClap.currentTime = 0
+        sClap.play()
+    }
+    
+})
+
+
